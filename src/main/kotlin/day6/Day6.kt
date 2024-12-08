3cfea47712581971f3743cbce35c7144f86ea4d2
Nicolò Sordoni <nicolo.sordoni@antreem.com>
2024-12-08 20:58:18 +0100

    Day 6

--- src/main/kotlin/day6/Day6.kt
+++ src/main/kotlin/day6/Day6.kt
@@ -1,14 +1,13 @@
- package day6
+package day6
 
 import utils.getDataLines
 
- fun main() {
+fun main() {
     val isDemo = false
     val scanner = getDataLines(6, if (isDemo) arrayOf("demo") else emptyArray())
 
     val input = Day6Input(scanner)
-    
-    val result = Day6Solution(input.matrix, input.initialGuardPosition).solve()
-    
-    println(result)
+
+    println(Day6Solution(input.matrix, input.initialGuardPosition).solvePart2())
+
 }
--- src/main/kotlin/day6/Day6Solution.kt
+++ src/main/kotlin/day6/Day6Solution.kt
@@ -1,142 +1,64 @@
 package day6
 
 import shared.*
-import javax.swing.text.Position
 
 class Day6Solution(private val matrix: Matrix<TerrainTile>, private val initialGuardPosition: Point) {
-    fun solve(): Int {
 
-        val movements = mutableListOf<Movement>()
+    fun solvePart2(): Int {
+
+
+        return getPath().count {
+            checkPathLoop(it)
+        }
+    }
+
+    private fun checkPathLoop(blockPosition: Point): Boolean {
 
         var guardPosition = initialGuardPosition
         var guardDirection: Direction = Up
 
-        val loopBlockPoints = HashSet<Point>()
+        val points = HashMap<Point, HashSet<Direction>>()
 
-        movements.add(Movement(guardDirection, guardPosition, false))
+
+        points[initialGuardPosition] = hashSetOf(guardDirection)
 
         while (matrix.inRange(guardPosition)) {
             val nextPoint = guardDirection.moveFrom(guardPosition)
 
 
-            if (matrix.inRange(nextPoint) && matrix[nextPoint] == TerrainTile.Obstruction) {
-                movements.last().endPoint = guardPosition
+            if (matrix.inRange(nextPoint) && (matrix[nextPoint] == TerrainTile.Obstruction || nextPoint == blockPosition)) {
                 guardDirection = guardDirection.turn90DegRight()
-                movements.add(Movement(guardDirection, guardPosition, false))
             } else {
                 guardPosition = nextPoint
 
-                if (findCircle(nextPoint, guardDirection, movements)) {
-                    val blockPosition = guardDirection.moveFrom(nextPoint)
 
-                    loopBlockPoints.add(blockPosition)
+
+                if (!points.containsKey(guardPosition)) {
+                    points[guardPosition] = hashSetOf(guardDirection)
+                } else {
+                    if (points[guardPosition]!!.contains(guardDirection)) {
+                        return true
+                    }
+                    points[guardPosition]!!.add(guardDirection)
                 }
-
             }
 
         }
 
-
-
-
-        return loopBlockPoints.size // Remove last position since is out of matrix
-
-    }
-
-
-    private fun findCircle(
-        currentPosition: Point,
-        currentDirection: Direction,
-        previousMovements: List<Movement>
-    ): Boolean {
-        var direction = currentDirection
-        var position = currentPosition
-
-        if (checkLoop(position, currentDirection, previousMovements)) {
-            return true
-        }
-
-        val movements = mutableListOf<Point>()
-
-        for (i in 0 until 1000) {
-            direction = direction.turn90DegRight()
-            val movement = nextMovement(position, direction)
-
-            if (movement.finishIntoEdge) {
-                return false
-            }
-
-            if (checkLoop(movement.endPoint!!, direction, previousMovements, currentPosition)) {
-                return true
-            }
-
-
-            position = movement.endPoint!!
-            movements.add(position)
-        }
 
 
 
         return false
-    }
-
-
-    private fun checkLoop(
-        currentPosition: Point,
-        currentDirection: Direction,
-        previousMovements: List<Movement>,
-        loopStartPosition: Point? = null
-    ): Boolean {
-        val rightDirection = currentDirection.turn90DegRight()
-
-        return previousMovements.any {
-
-            if (it.endPoint == null && it.direction == rightDirection  && loopStartPosition != null && rightDirection.canTravelFrom(
-                    currentPosition, loopStartPosition
-                )
-            ) {
-                return@any true
-            }
-
-            return@any it.endPoint != null && it.direction == rightDirection && rightDirection.canTravelFrom(
-                currentPosition,
-                it.endPoint!!
-            )
-        }
 
     }
 
-    private fun nextMovement(
-        startPosition: Point,
-        direction: Direction,
-    ): Movement {
-
-        var position = startPosition
-
-        while (matrix.inRange(position)) {
-            val nextPoint = direction.moveFrom(position)
-
-            if (!matrix.inRange(nextPoint)) {
-                return Movement(direction, startPosition, true, position)
-            }
-
-            if (matrix[nextPoint] == TerrainTile.Obstruction) {
-                return Movement(direction, startPosition, false, position)
-            } else {
-                position = nextPoint
-            }
-
-        }
-        return Movement(direction, startPosition, true, position)
-    }
-
-
-    fun solvePart1(): Int {
+    private fun getPath(): HashSet<Point> {
 
         var guardPosition = initialGuardPosition
         var guardDirection: Direction = Up
 
         val points = HashSet<Point>()
+
 
         points.add(initialGuardPosition)
 
@@ -154,17 +76,12 @@ class Day6Solution(private val matrix: Matrix<TerrainTile>, private val initialG
         }
 
 
+        // Remove last position since is out of matrix
+        points.removeIf {
+            !matrix.inRange(it)
+        }
 
-
-        return points.size - 1 // Remove last position since is out of matrix
+        return points
 
     }
 }
-
-
-data class Movement(
-    val direction: Direction,
-    val startPoint: Point,
-    val finishIntoEdge: Boolean,
-    var endPoint: Point? = null
-)
