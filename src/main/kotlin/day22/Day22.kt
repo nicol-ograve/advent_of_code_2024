af889c21ed4f450727a8b4e3827ded551cac42da
Nicolò Sordoni <nicolo.sordoni@antreem.com>
2024-12-27 21:47:00 +0100

    Day 22

--- src/main/kotlin/day22/Day22.kt
+++ src/main/kotlin/day22/Day22.kt
@@ -1,6 +1,7 @@
 package day22
 
 import utils.getDataScanner
+import java.lang.StringBuilder
 import java.util.Scanner
 
 fun main() {
@@ -14,18 +15,73 @@ fun main() {
         )
     }
 
+    val bestPricesMaps = hashMapOf<String, Array<Long>>()
 
 
-    val result = numbers.sumOf {
+    var index = -1
+    numbers.forEach {
+        index++
         var result = it
+        var price = result % 10
+
+        val last4 = arrayOf<Long?>(null, null, null, null)
+
         for (i in 0 until 2000) {
-            result = transform(result)
+
+            val newResult = transform(result)
+            val newPrice = newResult % 10
+
+            val priceChange = newPrice - price
+
+
+            price = newPrice
+            result = newResult
+
+
+            if (i < 3) {
+                last4[i + 1] = priceChange
+            } else {
+                val keyBuilder = StringBuilder()
+                for (k in 1..3) {
+                    last4[k - 1] = last4[k]
+                    keyBuilder.append(last4[k - 1].toString())
+                    keyBuilder.append("_")
+                }
+
+                last4[3] = priceChange
+                keyBuilder.append(priceChange.toString())
+
+                val key = keyBuilder.toString()
+
+                if (!bestPricesMaps.containsKey(key)) {
+                    bestPricesMaps[key] = Array(numbers.size) { 0L }
+                }
+
+                // wrong: if (bestPricesMaps[key]!![index] < price) {
+                if (bestPricesMaps[key]!![index] == 0L) {
+                    bestPricesMaps[key]!![index] = price
+                }
+
+            }
         }
-        result
+    }
+
+    var bestKey = ""
+    var bestSum = 0L
+
+    bestPricesMaps.keys.forEach { key ->
+        val totalSum = bestPricesMaps[key]!!.sum()
+
+        if (totalSum > bestSum) {
+            bestKey = key
+            bestSum = totalSum
+        }
+
     }
 
 
-    println(result)
+    println(bestKey)
+    println(bestSum)
 }
 
 
